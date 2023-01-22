5ca69808f1ed4d6f4acb62baa472af9aecef5eaf
Михаил Постников <mihha2397@mail.ru>
2023-01-22 22:15:39 +0300

    try2/1

--- yatube_api/api/permission.py
+++ yatube_api/api/permission.py
@@ -5,4 +5,4 @@ class IsAuthorPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.author == request.user  
+        return obj.author == request.user
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 from posts.models import Group, Post
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+
 
 from .permission import IsAuthorPermission
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
